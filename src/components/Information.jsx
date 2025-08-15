268f31c213ae46e1e480724751b822806d129c20
Hakan Şahin <hakonsahin@gmail.com>
2025-08-16 02:09:39 +0300

    Add files via upload

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,6 +1,6 @@
 function Information() {
   return (
-    <div className="flex flex-col items-center pt-25">
+    <div className="flex flex-col items-center pt-25 text-center">
       <h1 className="text-6xl">Hangi Sansar Salvo sinir krizisin?</h1>
       <p className="text-3xl pt-10">
         Verilen cevaplara göre hangi "Sansar Salvo" sinir krizi olduğunu öğren!
